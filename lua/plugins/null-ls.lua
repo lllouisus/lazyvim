28040067f74620b9ded455d9b58fd7fd31659772
xanmodlouis <xanmodlouis@gmail.com>
2023-03-27 21:30:44 +0800

    Add: null-ls Formatting Tool

---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,26 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "jay-babu/mason-null-ls.nvim",
+    "williamboman/mason.nvim",
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        -- null_ls.builtins.completion.spell,
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.cmake_lint,
+      },
+    })
+    require("null-ls.custom")
+    require("null-ls.markdownlint")
+
+    -- keymap
+    vim.keymap.set('n', '<Leader>f', ':lua vim.lsp.buf.format()<CR>', {})
+  end,
+}
